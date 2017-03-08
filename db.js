9b7ca6322adc9da88ab284f8c8f3a3e1bf422f0e
Andrew Tang <andrewtang@sbcglobal.net>
2017-03-08 17:56:53 -0600

    initial commit

---
+++ db.js
@@ -0,0 +1,148 @@
+var fs = require('fs');
+var readline = require('readline');
+var google = require('googleapis');
+var googleAuth = require('google-auth-library');
+
+var SCOPES = ['https://www.googleapis.com/auth/spreadsheets'];
+var TOKEN_DIR = (process.env.HOME || process.env.HOMEPATH ||
+    process.env.USERPROFILE) + '/.credentials/';
+var TOKEN_PATH = TOKEN_DIR + 'sheets.googleapis.com-nodejs-quickstart.json';
+
+/**
+ * Create an OAuth2 client with the given credentials, and then execute the
+ * given callback function.
+ *
+ * @param {Object} credentials The authorization client credentials.
+ * @param {function} callback The callback to call with the authorized client.
+ */
+function authorize(credentials, callback) {
+  var clientSecret = credentials.installed.client_secret;
+  var clientId = credentials.installed.client_id;
+  var redirectUrl = credentials.installed.redirect_uris[0];
+  var auth = new googleAuth();
+  var oauth2Client = new auth.OAuth2(clientId, clientSecret, redirectUrl);
+
+  // Check if we have previously stored a token.
+  fs.readFile(TOKEN_PATH, function(err, token) {
+    if (err) {
+      getNewToken(oauth2Client, callback);
+    } else {
+      oauth2Client.credentials = JSON.parse(token);
+      callback(oauth2Client);
+    }
+  });
+}
+
+exports.getAuth = function(callback) {
+  fs.readFile('client_secret.json', function(err, content) {
+    if (err) {
+      console.log('Error loading client secret file: ' + err);
+      return;
+    }
+    authorize(JSON.parse(content), auth => {
+      callback(auth);
+    });
+  });
+}
+// fs.readFile('client_secret.json', function processClientSecrets(err, content) {
+//   if (err) {
+//     console.log('Error loading client secret file: ' + err);
+//     return;
+//   }
+//   // Authorize a client with the loaded credentials, then call the
+//   // Google Sheets API.
+//   // authorize(JSON.parse(content), listMajors);
+//
+//
+//   authorize(JSON.parse(content), auth => {
+//     addRow(auth, [['Morton Shapiro', 'purple sweater']], res => {
+//       readData(auth, res => {
+//         console.log(res);
+//       });
+//     });
+//   });
+// });
+
+/**
+ * Get and store new token after prompting for user authorization, and then
+ * execute the given callback with the authorized OAuth2 client.
+ *
+ * @param {google.auth.OAuth2} oauth2Client The OAuth2 client to get token for.
+ * @param {getEventsCallback} callback The callback to call with the authorized
+ *     client.
+ */
+function getNewToken(oauth2Client, callback) {
+  var authUrl = oauth2Client.generateAuthUrl({
+    access_type: 'offline',
+    scope: SCOPES
+  });
+  console.log('Authorize this app by visiting this url: ', authUrl);
+  var rl = readline.createInterface({
+    input: process.stdin,
+    output: process.stdout
+  });
+  rl.question('Enter the code from that page here: ', function(code) {
+    rl.close();
+    oauth2Client.getToken(code, function(err, token) {
+      if (err) {
+        console.log('Error while trying to retrieve access token', err);
+        return;
+      }
+      oauth2Client.credentials = token;
+      storeToken(token);
+      callback(oauth2Client);
+    });
+  });
+}
+
+/**
+ * Store token to disk be used in later program executions.
+ *
+ * @param {Object} token The token to store to disk.
+ */
+function storeToken(token) {
+  try {
+    fs.mkdirSync(TOKEN_DIR);
+  } catch (err) {
+    if (err.code != 'EEXIST') {
+      throw err;
+    }
+  }
+  fs.writeFile(TOKEN_PATH, JSON.stringify(token));
+  console.log('Token stored to ' + TOKEN_PATH);
+}
+
+exports.readData = function(auth, callback) {
+  var sheets = google.sheets('v4');
+  sheets.spreadsheets.values.get({
+    auth: auth,
+    spreadsheetId: '1M4yBOZsr5dxVu1nCZDAdveVbdbC9U6jpCgNTPZY-Zq4',
+    range: 'Database!A2:B',
+  }, function(err, response) {
+    if (err) {
+      console.log('The API returned an error: ' + err);
+      return;
+    }
+    callback(response.values);
+  });
+}
+
+exports.addRow = function(auth, values, callback) {
+  var sheets = google.sheets('v4');
+  sheets.spreadsheets.values.append({
+    auth: auth,
+    spreadsheetId: '1M4yBOZsr5dxVu1nCZDAdveVbdbC9U6jpCgNTPZY-Zq4',
+    range: 'Database!A1',
+    valueInputOption: 'RAW',
+    resource: {
+      values: values
+    }
+  }, function(err, response) {
+    if(err) {
+      console.error(err);
+    } else {
+      console.log(response);
+      callback(response);
+    }
+  });
+}
---
+++ main.js
@@ -0,0 +1,123 @@
+'use strict';
+
+//Google Sheets API Stuff
+var db = require('./db.js');
+var fs = require('fs');
+var readline = require('readline');
+var google = require('googleapis');
+var googleAuth = require('google-auth-library');
+
+var SCOPES = ['https://www.googleapis.com/auth/spreadsheets'];
+var TOKEN_DIR = (process.env.HOME || process.env.HOMEPATH ||
+    process.env.USERPROFILE) + '/.credentials/';
+var TOKEN_PATH = TOKEN_DIR + 'sheets.googleapis.com-nodejs-quickstart.json';
+
+/**
+ * Create an OAuth2 client with the given credentials, and then execute the
+ * given callback function.
+ *
+ * @param {Object} credentials The authorization client credentials.
+ * @param {function} callback The callback to call with the authorized client.
+ */
+function authorize(credentials, callback) {
+  var clientSecret = credentials.installed.client_secret;
+  var clientId = credentials.installed.client_id;
+  var redirectUrl = credentials.installed.redirect_uris[0];
+  var auth = new googleAuth();
+  var oauth2Client = new auth.OAuth2(clientId, clientSecret, redirectUrl);
+
+  // Check if we have previously stored a token.
+  fs.readFile(TOKEN_PATH, function(err, token) {
+    if (err) {
+      getNewToken(oauth2Client, callback);
+    } else {
+      oauth2Client.credentials = JSON.parse(token);
+      callback(oauth2Client);
+    }
+  });
+}
+//End of Google Sheets API stuff
+
+let Wit = null;
+let interactive = null;
+try {
+  // if running from repo
+  Wit = require('../').Wit;
+  interactive = require('../').interactive;
+} catch (e) {
+  Wit = require('node-wit').Wit;
+  interactive = require('node-wit').interactive;
+}
+
+const accessToken = (() => {
+  if (process.argv.length !== 3) {
+    console.log('usage: node examples/quickstart.js <wit-access-token>');
+    process.exit(1);
+  }
+  return process.argv[2];
+})();
+
+// Quickstart example
+// See https://wit.ai/ar7hur/quickstart
+
+const firstEntityValue = (entities, entity) => {
+  const val = entities && entities[entity] &&
+    Array.isArray(entities[entity]) &&
+    entities[entity].length > 0 &&
+    entities[entity][0].value
+  ;
+  if (!val) {
+    return null;
+  }
+  return typeof val === 'object' ? val.value : val;
+};
+
+const actions = {
+  send(request, response) {
+    const {sessionId, context, entities} = request;
+    const {text, quickreplies} = response;
+    console.log('sending...', JSON.stringify(response));
+  },
+  getForecast({context, entities}) {
+    var location = firstEntityValue(entities, 'location');
+    if (location) {
+      context.forecast = 'sunny in ' + location; // we should call a weather API here
+      delete context.missingLocation;
+    } else {
+      context.missingLocation = true;
+      delete context.forecast;
+    }
+    return context;
+  },
+  saveName({context, entities}) {
+    var name = firstEntityValue(entities, 'contact');
+    console.log(context);
+    console.log(entities);
+    if (name) {
+      context.name = name;
+      console.log('Name: ' + name);
+    } else {
+      console.error('No name');
+    }
+    return context;
+  },
+  saveData({context, entities}) {
+    var feelings = [];
+    entities.feeling_intent.forEach(feeling => {
+      feelings.push(feeling.value);
+    });
+    context.feelings = feelings;
+    // console.log(context);
+    // console.log(entities);
+    db.getAuth(auth => {
+      console.log("Write Data: ", [[context.name, context.feelings.join(',')]]);
+      db.addRow(auth, [[context.name, context.feelings.join(',')]], result => {
+        console.log('data written!');
+      })
+    });
+    return context;
+  }
+};
+
+const client = new Wit({accessToken, actions});
+interactive(client);
