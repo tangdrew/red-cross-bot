edb2cd8635aa2a6f811c8d2aa9f50c68b588786f
Andrew Tang <andrewtang@sbcglobal.net>
2017-03-08 19:36:24 -0600

    fix wit pls

--- index.js
+++ index.js
@@ -323,35 +323,84 @@ const findOrCreateSession = (fbid) => {
   return sessionId;
 };
 
-// Our bot actions
+// // Our bot actions
+// const actions = {
+//   send({sessionId}, {text}) {
+//     // Our bot has something to say!
+//     // Let's retrieve the Facebook user whose session belongs to
+//     const recipientId = sessions[sessionId].fbid;
+//     if (recipientId) {
+//       // Yay, we found our recipient!
+//       // Let's forward our bot response to her.
+//       // We return a promise to let our bot know when we're done sending
+//       return fbMessage(recipientId, text)
+//       .then(() => null)
+//       .catch((err) => {
+//         console.error(
+//           'Oops! An error occurred while forwarding the response to',
+//           recipientId,
+//           ':',
+//           err.stack || err
+//         );
+//       });
+//     } else {
+//       console.error('Oops! Couldn\'t find user for session:', sessionId);
+//       // Giving the wheel back to our bot
+//       return Promise.resolve()
+//     }
+//   },
+//   // You should implement your custom actions here
+//   // See https://wit.ai/docs/quickstart
+// };
 const actions = {
-  send({sessionId}, {text}) {
-    // Our bot has something to say!
-    // Let's retrieve the Facebook user whose session belongs to
-    const recipientId = sessions[sessionId].fbid;
-    if (recipientId) {
-      // Yay, we found our recipient!
-      // Let's forward our bot response to her.
-      // We return a promise to let our bot know when we're done sending
-      return fbMessage(recipientId, text)
-      .then(() => null)
-      .catch((err) => {
-        console.error(
-          'Oops! An error occurred while forwarding the response to',
-          recipientId,
-          ':',
-          err.stack || err
-        );
-      });
-    } else {
-      console.error('Oops! Couldn\'t find user for session:', sessionId);
-      // Giving the wheel back to our bot
-      return Promise.resolve()
-    }
+  say(sessionId, context, message, cb) {
+      console.log(message);
+
+      // Bot testing mode, run cb() and return
+      if (require.main === module) {
+        cb();
+        return;
+      }
+
+      // Our bot has something to say!
+      // Let's retrieve the Facebook user whose session belongs to from context
+      // TODO: need to get Facebook user name
+      const recipientId = context._fbid_;
+      if (recipientId) {
+        // Yay, we found our recipient!
+        // Let's forward our bot response to her.
+        FB.fbMessage(recipientId, message, (err, data) => {
+          if (err) {
+            console.log(
+              'Oops! An error occurred while forwarding the response to',
+              recipientId,
+              ':',
+              err
+            );
+          }
+
+          // Let's give the wheel back to our bot
+          cb();
+        });
+      } else {
+        console.log('Oops! Couldn\'t find user in context:', context);
+        // Giving the wheel back to our bot
+        cb();
+      }
   },
-  // You should implement your custom actions here
-  // See https://wit.ai/docs/quickstart
-};
+  merge(sessionId, context, entities, message, cb) {
+    // Retrieve the location entity and store it into a context field
+    const loc = firstEntityValue(entities, 'location');
+    if (loc) {
+      context.loc = loc; // store it in context
+    }
+
+    cb(context);
+  },
+  error(sessionId, context, error) {
+    console.log(error.message);
+  }
+}
 
 // Setting up our bot
 const wit = new Wit({
